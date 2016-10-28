d4114277d74e0feec179cdc1da4364645345dd21
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-10-28 23:19:14 +0530

    Update upload2.php

--- upload2.php
+++ upload2.php
@@ -1,15 +1,12 @@
 <?php
-
 if(isset($_POST["submit"])){
-
     if($_FILES['fileToUpload']["tmp_name"] != ''){
     $file_name = $_FILES['fileToUpload']["name"];
     $array = explode(".",$file_name);
     $name= $array[0];
     $ext = $array[1];
     }
-    
-    if($ext == 'zip'){
+        if($ext == 'zip'){
         $path = 'C:/wamp/www/Web Jam/';
         $location = $path.$file_name;
         if(move_uploaded_file($_FILES['fileToUpload']["tmp_name"], $location)){
@@ -17,24 +14,18 @@ if(isset($_POST["submit"])){
             if($zip->open($location)){
                 $zip->extractTo($path);
                 $zip->close();
-        
-                error_reporting(0);
+                   error_reporting(0);
                 $files = scandir($path. $name);
                  if ($zip->open($file_name) === true) {
-     
-                     for($i = 0; $i < $zip->numFiles; $i++) { 
+                   for($i = 0; $i < $zip->numFiles; $i++) { 
                          $entry = $zip->getNameIndex($i);
                               if(preg_match('#\.(txt)$#i', $entry))
                               {
-
                                   $file = fopen($entry, "r");
-
-                                  $text = fgets($file);
+                                   $text = fgets($file);
                               // $arr1 = str_split($text);
                                   $arr1 = str_split($text);
-                    
-
-                                foreach ($arr1 as $key => $value) {
+                                 foreach ($arr1 as $key => $value) {
                                   if(ctype_digit($value)){
                                      $sum +=  $value;
                                      }else {
@@ -42,24 +33,15 @@ if(isset($_POST["submit"])){
                         }
                     }
                     $arr2 = array($text , $sum );
-
                     echo $sum;
                     echo '<br>';
-                   
-                     $file_new = fopen($entry.".csv", "w");
-
+                    $file_new = fopen($entry.".csv", "w");
                      foreach($arr2 as $number){
                           fputcsv($file_new, explode(',', $number));
-                       
                        }
                        fclose($file_new);
-
-
-
-               
-                    fclose($file);
-                    
-                } 
+                       fclose($file);
+                    } 
             }
         }
 
